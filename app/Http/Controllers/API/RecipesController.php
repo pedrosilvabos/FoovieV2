6670d730b14bc8aa2f81e6c272bcf332c0c4449b
JOAO PEDRO SILVA FARIA <j-pedro-faria@telecom.pt>
2021-02-21 15:15:36 -0100

    recepies accept ingredients, that have to come in as an array of the id of the ingredient

--- app/Http/Controllers/API/RecipesController.php
+++ app/Http/Controllers/API/RecipesController.php
@@ -20,7 +20,7 @@ class RecipesController extends Controller
     public function index()
     {
         $recipes = Recipes::all();
-        return response([ 'recipes' => ProjectResource::collection($recipes), 'message' => 'Retrieved recipes successfully!'], 200);
+        return response(['recipes' => ProjectResource::collection($recipes), 'message' => 'Retrieved recipes successfully!'], 200);
 
     }
 
@@ -29,7 +29,7 @@ class RecipesController extends Controller
      * here we have to pass in the ingredients as part of the arguments
      * and then we use it to create an array and store it as that
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -47,9 +47,12 @@ class RecipesController extends Controller
             return response(['error' => $validator->errors(), 'Validation Error']);
         }
 
+
+        $stack = explode(",", $data['ingredients']);
+
+
         $recipes = Recipes::create($data);
-
-
+        $recipes->ingredients = $stack;
         $recipes->save();
         return response(['project' => new ProjectResource($recipes), 'message' => 'Created successfully'], 201);
 
@@ -57,9 +60,9 @@ class RecipesController extends Controller
 
     /**
      * Display the specified resource.
-     * the id argument is auto magic, and thats nice
+     * the id argument is auto magic
      *
-     * @param  \App\Models\Recipes  $recipes
+     * @param \App\Models\Recipes $recipes
      * @return \Illuminate\Http\Response
      */
     public function show(Recipes $recipes, $id)
@@ -73,8 +76,8 @@ class RecipesController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Recipes  $recipes
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Recipes $recipes
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Recipes $recipes)
@@ -85,7 +88,7 @@ class RecipesController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Recipes  $recipes
+     * @param \App\Models\Recipes $recipes
      * @return \Illuminate\Http\Response
      */
     public function destroy(Recipes $recipes)
